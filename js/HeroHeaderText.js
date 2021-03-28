7a9fe9f8ac033d1d4c10604e6a99a4da35334807
Oscar <ozzy.coding@hotmail.com>
2021-03-28 16:57:00 +0100

    remove payapl

--- js/HeroHeaderText.js
+++ js/HeroHeaderText.js
@@ -1,12 +1,7 @@
-new Typed('.dev', {
-    strings: [
-        'Java',
-        'Python',
-        'HTML',
-        'CSS',
-    ],
-    smartBackspace: true,
-    loop: true,
-    typeSpeed: 85,
-    backSpeed: 85
+new Typed(".dev", {
+  strings: ["Java", "Python", "HTML", "CSS"],
+  smartBackspace: true,
+  loop: true,
+  typeSpeed: 85,
+  backSpeed: 85,
 });
